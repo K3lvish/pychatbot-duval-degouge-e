9348285b6c6c25e254568ba69361353d57e83855
QuentinDegouge <150792373+QuentinDegouge@users.noreply.github.com>
2023-11-26 15:28:29 +0100

    Update tf.py

--- tf.py
+++ tf.py
@@ -1,3 +1,12 @@
+from math import log
+import os
+def list_of_files(directory, extension):
+    files_names = []
+    for filename in os.listdir(directory):
+        if filename.endswith(extension):
+            files_names.append(filename)
+    return files_names
+    
 def tf(car):
     cara = car.split(" ")
     dico = {}
@@ -7,3 +16,18 @@ def tf(car):
         else:
             dico[el] += 1
     return dico
+
+def idf(rep):
+    dicoidf = {}
+    for el in list_of_files(rep, "txt"):
+        with open(rep + "/" + el, 'r') as file:
+            discours = file.read()
+            dico = tf(discours)
+            for keys in dico:
+                if keys not in dicoidf:
+                    dicoidf[keys] = 1
+                else:
+                    dicoidf[keys] += 1
+    for el in dicoidf:
+        dicoidf[el] = log(8/dicoidf[el])
+    return dicoidf
