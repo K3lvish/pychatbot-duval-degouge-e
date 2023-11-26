4ec606c6300489e597fb0e93f2fe77ab929a1f19
Kelvish <150790778+K3lvish@users.noreply.github.com>
2023-11-26 15:08:33 +0100

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,35 @@
+import os
+import re
+def list_of_files(directory, extension):
+    files_names = []
+    for filename in os.listdir(directory):
+        if filename.endswith(extension):
+            files_names.append(filename)
+    return files_names
+
+directory = "./speeches"
+files_names = list_of_files(directory, "txt")
+
+def dico_pres(tab):
+    dico = {}
+    for pres in tab:
+        nompres = re.sub(r'[0-9]', '', pres)[11:-4]
+        if nompres not in dico:
+            dico[nompres] = assigner(nompres)
+    return dico
+
+def assigner(nom):
+    prenom = ""
+    if nom == 'Chirac':
+        prenom = "Jacques"
+    elif nom == 'Giscard dEstaing':
+        prenom = 'Valérie'
+    elif nom == 'Hollande':
+        prenom = 'François'
+    elif nom == 'Macron':
+        prenom = 'Emmanuel'
+    elif nom == 'Mitterrand':
+        prenom = 'François'
+    elif nom == 'Sarkozy':
+        prenom = 'Nicolas'
+    return prenom
