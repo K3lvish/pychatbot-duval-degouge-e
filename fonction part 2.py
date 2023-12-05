5d2081fe9b274de198d3d1f2c18806acb8581eed
QuentinDegouge <150792373+QuentinDegouge@users.noreply.github.com>
2023-12-05 11:53:56 +0100

    Update fonction part 2.py

--- fonction part 2.py
+++ fonction part 2.py
@@ -7,4 +7,10 @@ def token(car):
             tabmot.remove(el)
     return tabmot
   
-
+def recherche(quest,dico):
+    present=[]
+    quest=token(quest)
+    for i in range(len(quest)):
+        if quest[i] in dico.keys() and quest[i] not in present:
+            present.append(quest[i])
+    return present
