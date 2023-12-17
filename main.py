b0070fcbf85cbfd1e65dd47ccbb0f9c882f4a76a
QuentinDegouge <150792373+QuentinDegouge@users.noreply.github.com>
2023-12-17 18:59:07 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,63 +1,65 @@
+from tf_idf_tfidf import *
 from functions import *
-from tfidf import *
 
-print(dico_pres(files_names))
-discours(files_names)
-liste_fichier = list_of_files('cleaned','txt')
+p1 = input("Voulez-vous voir les réponses de la première partie ? Répondez simplement par oui ou non.")
+if p1.lower() == 'oui':
+    print(dico_pres(noms_fichier))
+    discours(noms_fichier)
+    liste_fichier = liste_fichiers('cleaned','txt')
 
-matrice = tfidf('cleaned')
-dicomot = idf('cleaned')
-compteur = 0
-motuse = []
-for keys in dicomot:
-    dicomot[keys] = matrice[compteur]
-    compteur += 1
-for keys in dicomot:
-    occurences = 0
-    for el in dicomot[keys]:
-        if float(el) == 0.0 :
-            occurences += 1
-    if occurences == len(liste_fichier):
-        motuse.append(keys)
+    matrice = tfidf('cleaned')
+    dicomot = idf('cleaned')
+    compteur = 0
+    motut = []
+    for cles in dicomot:
+        dicomot[cles] = matrice[compteur]
+        compteur += 1
+    for cles in dicomot:
+        occurences = 0
+        for el in dicomot[cles]:
+            if float(el) == 0.0 :
+                occurences += 1
+        if occurences == len(liste_fichier):
+            motut.append(cles)
 
-maximum = max(max(matrice))
-for keys in dicomot:
-    if maximum == max(dicomot[keys]):
-        print(keys)
+    maximum = max(max(matrice))
+    for cles in dicomot:
+        if maximum == max(dicomot[cles]):
+            print(cles)
 
-print("Le mot le plus utilisé par Chirac est : ")
-maxi = 0
+    print("Le mot le plus utilisé par Chirac est : ")
+    maxi = 0
+    for el in liste_fichier:
+        if 'Chirac' == el[11:-5]:
+            with open('cleaned/' + el, 'r') as fichiers:
+                fichier = fichiers.read()
+                for cles in tf(fichier):
+                    if tf(fichier)[cles] > maxi and cles != '':
+                        maxi = tf(fichier)[cles]
+    for el in liste_fichier:
+        if 'Chirac' == el[11:-5]:
+            with open('cleaned/' + el, 'r') as fichiers:
+                fichier = fichiers.read()
+                for cles in tf(fichier):
+                    if maxi == tf(fichier)[cles] and cles != '':
+                        print(cles)
+    print("Les président ayant utilisé le mot 'nation' sont : ")
+    maxination = 0
+    presnation = 0
+    tabpres = []
 for el in liste_fichier:
-    if 'Chirac' == el[11:-5]:
-        with open('cleaned/' + el, 'r') as file:
-            fichier = file.read()
-            for keys in tf(fichier):
-                if tf(fichier)[keys] > maxi and keys != '':
-                    maxi = tf(fichier)[keys]
-for el in liste_fichier:
-    if 'Chirac' == el[11:-5]:
-        with open('cleaned/' + el, 'r') as file:
-            fichier = file.read()
-            for keys in tf(fichier):
-                if maxi == tf(fichier)[keys] and keys != '':
-                    print(keys)
-print("Les président ayant utilisé le mot 'nation' sont : ")
-maxination = 0
-presnation = 0
-tabpres = []
-for el in liste_fichier:
-    with open('cleaned/' + el, 'r') as file:
-        prestf = tf(file.read())
+    with open('cleaned/' + el, 'r') as fichiers:
+        prestf = tf(fichiers.read())
         nompres = re.sub(r'[0-9]', '', el)[11:-4]
-        if "nation" in prestf.keys():
+        if "nation" in prestf.cles():
             if nompres not in tabpres:
                 tabpres.append(nompres)
-        for keys in prestf:
-            if prestf[keys] > maxination:
-                maxination = prestf[keys]
+        for cles in prestf:
+            if prestf[cles] > maxination:
+                maxination = prestf[cles]
                 presnation = re.sub(r'[0-9]', '', el)[11:-4]
-for elem in tabpres:
-    print(elem)
+for el in tabpres:
+    print(el)
 print("Le président ayant le plus utilisé le mot 'nation' est " + presnation)
 
 print("Sakorzy est le premier président à parler d'écologie et de climat.")
@@ -65,12 +67,12 @@ print("Sakorzy est le premier président à parler d'écologie et de climat.")
 dicouse = {}
 for el in liste_fichier:
     with open('cleaned/' + el , 'r') as file:
-        speech = file.read()
-        for keys in tf(speech):
-            if keys not in dicouse:
-                dicouse[keys] = 1
+         speech = file.read()
+         for cles in tf(speech):
+            if cles not in dicouse:
+                dicouse[cles] = 1
             else:
-                dicouse[keys] += 1
-for keys in dicouse:
-    if dicouse[keys] == len(liste_fichier) and keys not in motuse:
-        print(keys)
+                dicouse[cles] += 1
+for cles in dicouse:
+    if dicouse[cles] == len(liste_fichier) and cles not in motut:
+        print(cles)
