2ea1e2cc9b0c3cc3006bde6e0da4e580006a1ef4
Kelvish <150790778+K3lvish@users.noreply.github.com>
2023-12-17 18:59:28 +0100

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,16 +1,20 @@
 import os
 import re
-def list_of_files(directory, extension):
-    files_names = []
-    for filename in os.listdir(directory):
-        if filename.endswith(extension):
-            files_names.append(filename)
-    return files_names
+def liste_fichiers(dossier, extension):
+    """str,str -> list
+    renvoie la liste des fichiers de directory de même extension que "extension" """
+    nom_fichier = []
+    for fichier in os.listdir(dossier):
+        if fichier.endswith(dossier):
+            nom_fichier.append(fichier)
+    return nom_fichier
 
-directory = "./speeches"
-files_names = list_of_files(directory, "txt")
+dossier = "./speeches"
+noms_fichier = liste_fichiers(dossier, "txt")
 
 def dico_pres(tab):
+    """list -> dict
+    renvoie un dictionnaire contenant le nom des présidents avec leur nom en objet et leur prénom en clé"""
     dico = {}
     for pres in tab:
         nompres = re.sub(r'[0-9]', '', pres)[11:-4]
@@ -19,6 +23,8 @@ def dico_pres(tab):
     return dico
 
 def assigner(nom):
+    """ str -> str
+    assigne au président son prénom"""
     prenom = ""
     if nom == 'Chirac':
         prenom = "Jacques"
@@ -35,19 +41,21 @@ def assigner(nom):
     return prenom
 
 def polir(txt):
-    text = txt.replace(',', ' ')
-    text = text.replace('?', ' ')
-    text = text.replace('.', ' ')
-    text = text.replace(';', ' ')
-    text = text.replace(':', ' ')
-    text = text.replace('!', ' ')
-    text = text.replace('-', ' ')
-    text = text.replace("'", ' ')
+    """str -> str
+    renvoie le texte entré sans aucune pontuation et avec un espace entre chaque mot"""
+    caracteres_a_remplacer = [',', '?', '.', ';', ':', '!', '-', "'", '"']
+    caractere_de_remplacement = ' '
+    text=txt
+    for caractere in caracteres_a_remplacer:
+        text = text.replace(caractere, caractere_de_remplacement)
     return text
 
+
 def discours(tab):
+    """ "polis" les discours
+    et les écrit dans le dossier clean"""
     for el in tab:
-        with open("speeches/" + el, 'r') as raw:
-            discours = polir(raw.read().lower())
-            with open("cleaned/" + el, 'w') as clean:
-                clean.write(discours)
+        with open("speeches/" + el, 'r') as brut:
+            discours = polir(brut.read().lower())
+            with open("cleaned/" + el, 'w') as poli:
+                poli.write(discours)
