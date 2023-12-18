31dc25fa4e11443b0488cce6779752a637072dd0
Kelvish <150790778+K3lvish@users.noreply.github.com>
2023-12-18 21:39:52 +0100

    Update fonctions.py

--- fonctions.py
+++ fonctions.py
@@ -3,14 +3,11 @@ import re
 def liste_fichiers(dossier, extension):
     """str,str -> list
     renvoie la liste des fichiers de directory de même extension que "extension" """
-    nom_fichier = []
+    noms_fichier = []
     for fichier in os.listdir(dossier):
-        if fichier.endswith(dossier):
-            nom_fichier.append(fichier)
-    return nom_fichier
-
-dossier = "./speeches"
-noms_fichier = liste_fichiers(dossier, "txt")
+        if fichier.endswith(extension):
+            noms_fichier.append(fichier)
+    return noms_fichier
 
 def dico_pres(tab):
     """list -> dict
